830eb92665921ba0a536ecc17d7dc9f30498390e
Shubham Kamble <46300889+shubhamkamble20@users.noreply.github.com>
2024-05-12 20:25:50 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket-project-terraform-batch-23"
     key = "main"
-    region = "us-east-1"
+    region = "us-west-2"
     dynamodb_table = "my-dynamodb-table"
   }
 }
